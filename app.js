419fce5c06a73c1d17fc6090d3fd9f28e52e00e5
Kai Dewey <kade6763@colorad.edu>
2021-02-05 11:19:47 +0900

    add express node package

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 8000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.use(express.static('public'))
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`CU app listening at http://localhost:${port}`)
 });
-
