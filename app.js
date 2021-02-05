ec93b844f66bede7df3580df10de4b1f5af28448
Kai Dewey <kade6763@colorad.edu>
2021-02-05 11:40:00 +0900

    update eslint rules

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.listen(port, () => {
-  console.log(`CU app listening at http://localhost:${port}`)
+  console.log(`CU app listening at http://localhost:${port}`);
 });
