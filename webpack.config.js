1580586b6141913c813ded2e1311c87b83c53744
Kai Dewey <kade6763@colorad.edu>
2021-02-11 12:15:50 +0900

    fix formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -65,5 +65,5 @@ module.exports = {
         type: 'asset/resource',
       },
     ],
-  }
-}
+  },
+};
