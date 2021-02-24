ae5c31c90ef0abf6d0929eb187192c6b54ecc163
Kai Dewey <kade6763@colorad.edu>
2021-02-24 11:20:16 +0900

    cleanup
    cleanup views and dirs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
@@ -21,38 +20,6 @@ module.exports = {
         { from: 'public/images', to: 'images' },
       ],
     }),
-    /*
-    new HtmlWebpackPlugin({
-      title: 'Home',
-      filename: 'index.html',
-      template: 'views/home.hbs',
-      chunks: ['index'],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'About',
-      filename: 'about.html',
-      template: 'views/about.hbs',
-      chunks: ['main'],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Login',
-      filename: 'login.html',
-      template: 'views/login.hbs',
-      chunks: ['main'],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Register',
-      filename: 'register.html',
-      template: 'views/register.hbs',
-      chunks: ['main', 'signup'],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Add Event',
-      filename: 'addEvent.html',
-      template: 'views/addEvent.hbs',
-      chunks: ['main'],
-    }),
-    */
   ],
   output: {
     filename: 'js/[name].bundle.js',
