cd29c11e84b7db992b1843a9c9958d7f85f014a4
Kai Dewey <kade6763@colorad.edu>
2021-03-02 19:56:45 +0900

    event detail view

--- routes/event.js
+++ routes/event.js
@@ -14,6 +14,10 @@ router.get('/list', async (req, res, next) => {
   }
 });
 
+router.get('/', (req, res) => {
+  res.redirect('/event/list');
+});
+
 router.get('/create', (req, res) => {
   res.render('event/create');
 });
@@ -31,7 +35,6 @@ router.post('/create', async (req, res, next) => {
     const queryString = 'INSERT INTO events (summary, description, time_start, all_day, tags) VALUES ($1, $2, $3, $4, $5) RETURNING event_id';
     const values = [summary, description, startTime, allDay, tags];
     const result = await pool.query(queryString, values);
-    console.log(result);
     const eventId = result.rows[0].event_id;
     res.redirect(`/event/${eventId}`);
   } catch (err) {
@@ -39,8 +42,26 @@ router.post('/create', async (req, res, next) => {
   }
 });
 
-router.get('/:eventId', (req, res) => {
-  res.render('event/detail', { id: req.params.eventId });
+router.get('/:eventId', async (req, res, next) => {
+  let id = req.params.eventId;
+  if (isNaN(id)) {
+    next();
+    return;
+  }
+
+  try {
+    const queryString = 'SELECT summary, description, time_start, all_day, tags FROM events WHERE event_id = $1';
+    const results = await pool.query(queryString, [id]);
+    const rows = results.rows;
+    if (rows.length === 0) {
+      next();
+      return;
+    }
+    let event = rows[0];
+    res.render('event/detail', {event});
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
