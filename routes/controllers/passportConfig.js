486d200e94e4c94d4ab83c327c8a04970930651d
Kai Dewey <kade6763@colorad.edu>
2021-03-02 20:15:37 +0900

    cleanup passportConfig

--- routes/controllers/passportConfig.js
+++ routes/controllers/passportConfig.js
@@ -4,8 +4,15 @@ const { pool } = require('./dbConfig');
 
 function initialize(passport) {
   const authenticateUser = (email, password, done) => {
-    pool.query('SELECT * FROM users WHERE email = $1', [email])
-      .then((results) => {
+    pool.query(
+      'SELECT * FROM users WHERE email = $1', [email], (queryErr, results) => {
+        if (queryErr) {
+          done(queryErr);
+          return;
+        }
+
+        console.log(results.rows);
+
         if (results.rows.length === 0) {
           done(null, false, { message: 'There is no account with this email!' });
           return;
@@ -13,17 +20,21 @@ function initialize(passport) {
 
         const user = results.rows[0];
 
-        bcrypt.compare(password, user.password)
-          .then((isMatch) => {
-            if (isMatch) {
-              done(null, user);
-            } else {
-              done(null, false, { message: 'Password is not correct!' });
-            }
-          })
-          .catch((err) => done(err));
-      })
-      .catch((err) => done(err));
+        bcrypt.compare(password, user.password, (validationErr, isMatch) => {
+          if (validationErr) {
+            done(validationErr);
+            return;
+          }
+
+          if (!isMatch) {
+            done(null, false, { message: 'Password is not correct!' });
+            return;
+          }
+
+          done(null, user);
+        });
+      },
+    );
   };
 
   passport.use(
@@ -41,7 +52,9 @@ function initialize(passport) {
   passport.deserializeUser((id, done) => {
     pool.query(
       'SELECT * FROM users WHERE id = $1', [id], (err, results) => {
-        if (err) return done(err);
+        if (err) {
+          throw err;
+        }
 
         return done(null, results.rows[0]);
       },
