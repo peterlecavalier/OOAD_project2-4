2b122377717393753aebf7ef88aaf698af8ed86d
Kai Dewey <kade6763@colorad.edu>
2021-03-03 11:02:13 +0900

    client-side validation

--- routes/controllers/auth.js
+++ routes/controllers/auth.js
@@ -6,20 +6,7 @@ async function createUser(req, res, next) {
     username,
     email,
     password,
-    passwordConfirm,
   } = req.body;
-
-  if (!username || !email || !password || !passwordConfirm) {
-    res.render('user/register', { message: 'Please complete all fields.' });
-  }
-
-  if (password.length < 6) {
-    res.render('user/register', { message: 'The password should be at least 6 characters.' });
-  }
-
-  if (password !== passwordConfirm) {
-    res.render('user/register', { message: 'The passwords do not match.' });
-  }
 
   // Form validation has passed
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -33,6 +20,7 @@ async function createUser(req, res, next) {
     const insertQuery = await pool.query(`INSERT INTO users (username, email, password)
                       VALUES ($1, $2, $3)
                       RETURNING id`, [username, email, hashedPassword]);
+
     console.log(insertQuery.rows);
     res.redirect('/');
   } catch (err) {
