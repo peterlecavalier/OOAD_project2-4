0f4bc200b82ea2cd207f1df83a5c71af6eb565a7
lizzzshan <odysuessrox@gmail.com>
2022-02-06 12:30:11 -0700

    committing current changes Item.java

---
+++ Desktop/OOAD/project 2/Item.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Item{
+    //instance variables
+    public String name;
+    private Double purchasePrice;
+    public Double listPrice;
+    public String newOrUsed;
+    public String dayArrived;
+    public String condition;
+    public Double salePrice;
+    public String daySold; 
+
+    //TO DO: add constructor
+
+    public String Music(String band, String album){
+
+    }
+
+
+}
