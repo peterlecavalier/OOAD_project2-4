f613bcbde0c4f0e9855cc62fbe1c36934f744371
Peter LeCavalier <pele6150@colorado.edu>
2022-02-06 13:16:10 -0700

    Initial CashRegister, Customer, and Store

--- CashRegister.java
+++ CashRegister.java
@@ -1,4 +1,10 @@
 public class CashRegister {
     private Double moneyAmt;
     private Double moneyFromBank;
+
+    private getMoney(){
+
+    }
+
+    // CashRegister constructor
 }
--- Item.java
+++ Item.java
@@ -1,3 +1,5 @@
+package FNMS;
+
 import java.util.Arrays;
 import java.util.List;
 
